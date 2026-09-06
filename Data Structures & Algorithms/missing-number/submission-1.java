740f267ad562fa6b7c267baf5d0ae3ceee56c2ab
Mihajlo Radojevic <mradojevic016@gmail.com>
2026-09-06 21:16:21 +0200

    Add: missing-number - submission-1

---
+++ Data Structures & Algorithms/missing-number/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static int missingNumber(int[] nums) {
+
+        int sum = 0;
+        for (int i = 1; i <= nums.length; i++) {
+            sum += i;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            sum -= nums[i];
+        }
+        return sum;
+    }
+}
